9faded646f8c46a150f2b883d18e8a35c81688a5
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-07-16 11:26:41 -0500

    Hello, World!

---
+++ go.mod
@@ -0,0 +1,10 @@
+module html-quickstart
+
+go 1.26.2
+
+require (
+	github.com/a-h/templ v0.3.1020
+	github.com/labstack/echo/v5 v5.3.0
+)
+
+require golang.org/x/time v0.15.0 // indirect
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html-quickstart/html"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c *echo.Context) error {
+		return html.Page().Render(c.Request().Context(), c.Response())
+	})
+
+	if err := e.Start(":1717"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
