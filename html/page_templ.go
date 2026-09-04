3a826470ff52df72098919bc369c367866a57846
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-09-04 11:10:06 -0500

    ho my gawd bruh

--- html/page_templ.go
+++ html/page_templ.go
@@ -8,6 +8,8 @@ package html
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/tediferous/wordmark/font"
+
 func Page() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
